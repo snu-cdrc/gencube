76511a3b2a3ee9ba04f0d4ac84996e66c6ef4147
keun-hong <newhong@snu.ac.kr>
2025-03-21 15:52:35 +0900

    Update

--- gencube/gencube_info.py
+++ gencube/gencube_info.py
@@ -6,11 +6,6 @@ from .utils import (
     delete_file,
 )
 
-from .utils import (
-    get_entrez_info,
-    delete_file,
-)
-
 ## gencube info ------------------------------
 def info (
     info_save
@@ -19,32 +14,31 @@ def info (
     if not info_save:
         home_dir = Path.home()
         config_file = home_dir / '.gencube_entrez_info'
-        
+
         print(
             'The submitted email and API key are as follows.\n'
             '\n'
             f'Path: {config_file}'
             )
-        
+
         # Show the previously submitted information
         with open(config_file, 'r') as in_f:
             for line in in_f:
                 if '#' not in line:
                     print(line.replace('\n', ''))
-            print('')                
-                    
+            print('')
+
         while True:
             question = input("Would you like to modify them? (type 'yes or 'no'): ").strip()
-        
+
             if question.lower() in ['yes', 'y']:
                 delete_file(config_file)
                 get_entrez_info()
                 break
-            
+
             elif question.lower() in ['no', 'n']:
                 break
-    
-    
 
 
-    
+
+
