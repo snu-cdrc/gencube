87977a99111b553245093fbfedc59e4149536972
keun-hong <newhong@snu.ac.kr>
2025-07-07 04:06:58 +0900

    Update

--- gencube/gencube_genome.py
+++ gencube/gencube_genome.py
@@ -74,10 +74,9 @@ def genome (
 
         # Download genome files
         if download:
-            download_genome(df_genome_plus, database, dic_genark_meta, dic_ensembl_meta, recursive)
 
-            #dic_download = download_genome(df_genome_plus, database, dic_genark_meta, dic_ensembl_meta, recursive)
+            dic_download = download_genome(df_genome_plus, database, dic_genark_meta, dic_ensembl_meta, recursive)
             # Change chromosome label style
-            #convert_chr_label_genome(df_genome_plus, dic_download, chr_style, masking, compresslevel, recursive)
+            convert_chr_label_genome(df_genome_plus, dic_download, chr_style, masking, compresslevel, recursive)
 
 
